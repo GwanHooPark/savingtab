1b154c788167f4bed25b19b4109481d9988d7b1b
ghpark <ghpark@daou.co.kr>
2020-04-24 15:23:02 +0900

    123

---
+++ src/components/TabGroup.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <div class="etabs-tabgroup">
+            <div class="etabs-tabs"></div>
+            <div class="etabs-buttons"></div>
+        </div>
+        <div class="etabs-views">
+        </div>
+    </div>
+</template>
+<script>
+    import TabGroup from 'electron-tabs'
+    export default {
+        name: 'TabGroup',
+        mounted() {
+            let tabGroup = new TabGroup({
+                ready : function(){
+                    console.log("ready");
+                    console.log(document.getElementsByTagName("webview"));
+                    let webview = document.getElementsByTagName("webview");
+                    for(const p of webview){
+                        console.log(p);
+                       // webview.setAttribute("allowpopups")
+                    }
+                }
+            });
+            tabGroup.on("tab-added", (tab) => {
+                console.log(tab);
+                //tab.webview.setAttribute("allowpopups","");
+            })
+            tabGroup.addTab({
+                title: "Portal",
+                src: "https://portal.daou.co.kr",
+                visible: true,
+                webviewAttributes : {
+                    "allowpopups" : true
+                }
+            });
+            tabGroup.addTab({
+                title: "Jira",
+                src: "https://issue.daou.co.kr",
+                visible: true,
+                active : true,
+                webviewAttributes : {
+                    "allowpopups" : true
+                }
+            });
+            tabGroup.addTab({
+                title: "Stash",
+                src: "https://stash.daou.co.kr",
+                visible: true,
+                webviewAttributes : {
+                    "allowpopups" : true
+                }
+            });
+        }
+    };
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <TabGroup/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import TabGroup from "@/components/TabGroup.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    TabGroup
   }
 };
 </script>
